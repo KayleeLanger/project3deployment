901b481e9152eea9ba7196ef8ab9c25a1706e2bb
KayleeLanger <klanger@tamu.edu>
2025-03-26 16:15:46 -0500

    making more database changes

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 
+const isRender = process.env.RENDER === "true";
 // Create pool
 const pool = new Pool({
     user: process.env.PSQL_USER,
@@ -17,7 +18,8 @@ const pool = new Pool({
     database: process.env.PSQL_DATABASE,
     password: process.env.PSQL_PASSWORD,
     port: process.env.PSQL_PORT,
-    ssl: {rejectUnauthorized: false}
+    sl: {rejectUnauthorized: false}
+    //ssl: isRender ? { rejectUnauthorized: false } : false
 });
 
 process.on('SIGINT', function() {
@@ -41,20 +43,21 @@ app.get('/api/xreport', async (req, res) => {
     try {
         const query = `
             SELECT EXTRACT(HOUR FROM o.orderDate) AS hour,
-                ROUND(SUM(oi.quantity * d.drinkPrice)::numeric, 2) AS total_sales,
-                COUNT(o.orderId) AS total_orders,
-                COUNT(DISTINCT o.employeeId) AS total_employees,
-                SUM(oi.quantity) AS total_items,
-                COUNT(CASE WHEN o.orderId % 2 != 0 THEN 1 END) AS apple_pay_count,
-                COUNT(CASE WHEN o.orderId % 2 = 0 THEN 1 END) AS card_count
-            FROM orders o
-            JOIN order_items oi ON o.orderId = oi.orderId
-            JOIN drink d ON oi.drinkId = d.drinkId
-            WHERE o.orderDate::DATE = CURRENT_DATE
-            AND EXTRACT(HOUR FROM o.orderDate) < EXTRACT(HOUR FROM CURRENT_TIMESTAMP)
-            GROUP BY hour
-            ORDER BY hour;
+         ROUND(SUM(oi.quantity * d.drinkPrice)::numeric, 2) AS total_sales,
+         COUNT(o.orderId) AS total_orders,
+         COUNT(DISTINCT o.employeeId) AS total_employees,
+         SUM(oi.quantity) AS total_items,
+         COUNT(CASE WHEN o.orderId % 2 != 0 THEN 1 END) AS apple_pay_count,
+         COUNT(CASE WHEN o.orderId % 2 = 0 THEN 1 END) AS card_count
+  FROM orders o
+  JOIN order_items oi ON o.orderId = oi.orderId
+  JOIN drink d ON oi.drinkId = d.drinkId
+  WHERE o.orderDate::DATE = '2024-03-05'
+    AND EXTRACT(HOUR FROM o.orderDate) < EXTRACT(HOUR FROM CURRENT_TIMESTAMP)
+  GROUP BY hour
+  ORDER BY hour;
         `;
+        //change to current hour possibly?
         const result = await pool.query(query);
         res.json(result.rows);
     } catch (err) {
@@ -77,7 +80,7 @@ app.get('/api/zreport', async (req, res) => {
             FROM orders o 
             JOIN order_items oi ON o.orderId = oi.orderId 
             JOIN drink d ON oi.drinkId = d.drinkId 
-            WHERE o.orderDate::DATE = CURRENT_DATE;
+            WHERE o.orderDate::DATE = '2024-03-05';
         `;
         const result = await pool.query(query);
         res.json(result.rows);
